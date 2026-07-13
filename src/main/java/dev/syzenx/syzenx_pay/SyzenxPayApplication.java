b3a017c91862aba681b5102a8b892da965442bf8
Syzen-x <syzenx63@gmail.com>
2026-07-12 21:58:03 -0500

    chore: initial commit with SpringBoot base architecture

---
+++ src/main/java/dev/syzenx/syzenx_pay/SyzenxPayApplication.java
@@ -0,0 +1,13 @@
+package dev.syzenx.syzenx_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyzenxPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyzenxPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/syzenx/syzenx_pay/SyzenxPayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.syzenx.syzenx_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyzenxPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
